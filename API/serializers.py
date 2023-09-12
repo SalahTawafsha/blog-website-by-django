892aa6e5b41f02f22ac24f6a26c3089cdbf48a29
salah tawafsha <salaht321@gmail.com>
2023-09-12 21:56:03 +0300

    forth commit

--- API/serializers.py
+++ API/serializers.py
@@ -4,27 +4,10 @@ from rest_framework.serializers import ModelSerializer
 from posts.models import Post, Comment
 
 
-def encrypt_function(string):
-    encrypt_string = ""
-    for char in string:
-        encrypt_string += chr(ord(char) + 2)
-
-    return encrypt_string
-
-
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-
-    def to_representation(self, instance):
-        # Retrieve the original representation of the instance
-        original_representation = super().to_representation(instance)
-
-        # Encrypt the password field (replace 'password' with the actual field name)
-        original_representation['password'] = encrypt_function(original_representation['password'])
-
-        return original_representation
 
 
 class PostSerializer(ModelSerializer):
